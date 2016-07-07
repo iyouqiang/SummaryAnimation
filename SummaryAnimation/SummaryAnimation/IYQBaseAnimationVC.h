ae56b41929c070d34bded61e5cb149274d017aff
iyouqiang <iyouqiang@icloud.com>
2016-07-07 15:10:08 +0800

    初始化
    收集动画

---
+++ SummaryAnimation/SummaryAnimation/IYQBaseAnimationVC.h
@@ -0,0 +1,13 @@
+//
+//  IYQBaseAnimationVC.h
+//  SummaryAnimation
+//
+//  Created by chmtech003 on 16/7/7.
+//  Copyright © 2016年 gongyouqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IYQBaseAnimationVC : UIViewController
+
+@end
---
+++ SummaryAnimation/SummaryAnimation/IYQBaseAnimationVC.m
@@ -0,0 +1,39 @@
+//
+//  IYQBaseAnimationVC.m
+//  SummaryAnimation
+//
+//  Created by chmtech003 on 16/7/7.
+//  Copyright © 2016年 gongyouqiang. All rights reserved.
+//
+
+#import "IYQBaseAnimationVC.h"
+
+@interface IYQBaseAnimationVC ()
+
+@end
+
+@implementation IYQBaseAnimationVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
