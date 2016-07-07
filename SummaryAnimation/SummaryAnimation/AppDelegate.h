8bb6a3861ae7855468943f590f3809ada2a3e098
iyouqiang <iyouqiang@icloud.com>
2016-07-07 11:14:48 +0800

    Initial commit

---
+++ SummaryAnimation/SummaryAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SummaryAnimation
+//
+//  Created by chmtech003 on 16/7/7.
+//  Copyright © 2016年 gongyouqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SummaryAnimation/SummaryAnimation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SummaryAnimation
+//
+//  Created by chmtech003 on 16/7/7.
+//  Copyright © 2016年 gongyouqiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
